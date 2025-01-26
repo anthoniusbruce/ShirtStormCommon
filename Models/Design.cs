1d41691fb71887ffb28e13d897e7bc66dfda6718
Andy Collins <anthonius.bruce@gmail.com>
2025-01-26 13:57:52 -0500

    Add project files.

---
+++ Models/Design.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShirtStormCommon.Models
+{
+    public class Design
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public Guid? ImageId { get; set; }
+        [Required]
+        public string? Title { get; set; }
+        [Required]
+        public string? Description {  get; set; }
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
+        [Required]
+        public bool DisplayOnFrontPage {  get; set; }
+    }
+}
---
+++ Models/Image.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShirtStormCommon.Models
+{
+    public class Image
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public byte[]? Bytes { get; set; }
+    }
+}
