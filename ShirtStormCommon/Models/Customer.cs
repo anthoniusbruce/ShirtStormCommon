c83f9e58f45919ebf633c92c49d987299dbfa368
Andy Collins <anthonius.bruce@gmail.com>
2025-02-06 19:20:02 -0500

    added customer model and bumped version to 1.0.3.0

---
+++ ShirtStormCommon/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShirtStormCommon.Models
+{
+    public class Customer
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public required string IdentityEmail { get; set; }
+        public required string FirstName { get; set; }
+        public required string Surname { get; set; }
+        public required string DisplayName { get; set; }
+
+        public bool IsAMember { get; set; }
+    }
+}
